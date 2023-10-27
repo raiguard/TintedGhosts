5a86c6c90da5dfaf05db4952c7411697065b697c
SetazeR <we.are@setazer.us>
2023-10-27 15:56:20 +0500

    AAI Industries teal tint defaults

--- settings.lua
+++ settings.lua
@@ -3,12 +3,12 @@ data:extend({
     type = "color-setting",
     name = "tg-ghost-tint",
     setting_type = "startup",
-    default_value = { g = 1, b = 1, a = 0.3 },
+    default_value = {r = 0.3, g = 0.6, b = 0.6, a = 0.3},
   },
   {
     type = "color-setting",
     name = "tg-tile-ghost-tint",
     setting_type = "startup",
-    default_value = { g = 1, b = 1, a = 0.4 },
+    default_value = {r = 0.4, g = 0.8, b = 0.8, a = 0.4},
   },
 })
